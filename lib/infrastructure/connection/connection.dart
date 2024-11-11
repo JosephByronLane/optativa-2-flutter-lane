765b5f9dc69ef387116f584ad06271d8f50da9fe
= <=>
2024-11-10 19:02:17 -0600

    im done lmao

--- lib/infrastructure/connection/connection.dart
+++ lib/infrastructure/connection/connection.dart
@@ -5,20 +5,35 @@ import 'package:http/http.dart' as http;
 class Connection implements Iconnection {
   @override
   Future<O> get<O>(String url, {Map<String, String>? headers}) async {
-    final response = await http.get(Uri.parse(url), headers: headers);
-    return jsonDecode(response.body) as O;
+    try {
+      final response = await http.get(Uri.parse(url), headers: headers);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body) as O;
+      } else {
+        throw Exception('Failed to load data: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Network error: $e');
+    }
   }
 
   @override
   Future<O> post<O, I>(String url, I data,
       {Map<String, String>? headers}) async {
-    final response = await http.post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode(data),
-    );
-      return jsonDecode(response.body) as O;
-    
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: headers,
+        body: jsonEncode(data),
+      );
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body) as O;
+      } else {
+        throw Exception('Failed to authenticate: ${response.statusCode} - ${response.body}');
+      }
+    } catch (e) {
+      throw Exception('Network error: $e');
+    }
   }
 
   @override
@@ -27,7 +42,7 @@ class Connection implements Iconnection {
   }
 
   @override
-  put<O,I>(String url,I data, {Map<String, String>? headers}) {
+  put<O, I>(String url, I data, {Map<String, String>? headers}) {
     throw UnimplementedError();
   }
 }
