0cedcf9aacd0f1075a751366efeec4e03fb58ab1
= <=>
2024-11-11 15:11:58 -0600

    forgot to add delete button

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 
-class CartScreen extends StatelessWidget {
-  final LocalStorage storage = LocalStorage('localstorage_app');
+class CartScreen extends StatefulWidget {
+  const CartScreen({Key? key}) : super(key: key);
 
-  CartScreen({super.key});
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  final LocalStorage storage = LocalStorage('localstorage_app');
+  List<dynamic> cartItems = [];
+  double totalPrice = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCartItems();
+  }
+
+  Future<void> _loadCartItems() async {
+    await storage.ready;
+    setState(() {
+      cartItems = storage.getItem('cart') ?? [];
+      totalPrice = _calculateTotalPrice();
+    });
+  }
+
+  double _calculateTotalPrice() {
+    return cartItems.fold(0, (sum, item) => sum + (item['totalPrice'] as double));
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      cartItems.removeAt(index);
+      totalPrice = _calculateTotalPrice();
+      storage.setItem('cart', cartItems); 
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,71 +46,59 @@ class CartScreen extends StatelessWidget {
         title: const Text('Cart'),
         backgroundColor: Colors.blue,
       ),
-      body: FutureBuilder(
-        future: storage.ready,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            List<dynamic> cartItems = storage.getItem('cart') ?? [];
-
-            if (cartItems.isEmpty) {
-              return const Center(
-                child: Text(
-                  'Carrito vacio.',
-                  style: TextStyle(fontSize: 20),
+      body: cartItems.isEmpty
+          ? const Center(
+              child: Text(
+                'Carrito vacio.',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cartItems.length,
+                    itemBuilder: (context, index) {
+                      var item = cartItems[index];
+                      return ListTile(
+                        leading: Image.network(
+                          item['image'],
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain,
+                        ),
+                        title: Text(
+                          item['name'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text('Price: \$${item['price']}'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Quantity: ${item['quantity']}'),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () => _removeItem(index),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              );
-            } else {
-              double totalPrice = cartItems.fold(
-                0,
-                (sum, item) => sum + (item['totalPrice'] as double),
-              );
-
-              return Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: cartItems.length,
-                      itemBuilder: (context, index) {
-                        var item = cartItems[index];
-                        return ListTile(
-                          leading: Image.network(
-                            item['image'],
-                            height: 100,
-                            width: 100,
-                            fit: BoxFit.contain,
-
-                          ),
-                          title: Text(
-                            item['name'],
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text('Price: \$${item['price']}'),
-                          trailing: Text('Quantity: ${item['quantity']}'),
-                        );
-                      },
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Total a pagar: \$${totalPrice.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const Divider(),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Total a pagar: \$${totalPrice.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            }
-          }
-        },
-      ),
+                ),
+              ],
+            ),
     );
   }
 }
